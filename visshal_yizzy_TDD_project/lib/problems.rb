1d19d4b69dc4980b5f6003bd803d0c7deebfeede
ypeikes18 <ypeikes18@gmail.com>
2021-10-28 13:48:45 -0400

    add two_sum

--- visshal_yizzy_TDD_project/lib/problems.rb
+++ visshal_yizzy_TDD_project/lib/problems.rb
@@ -3,3 +3,27 @@ def my_uniq(arr)
     arr.each {|ele| new_arr << ele unless new_arr.include?(ele)}
     new_arr
 end
+
+
+class Array
+
+    def two_sum
+        arr = []
+        (0...self.length - 1).each do |i| 
+          (i + 1...self.length).each do |j|
+            arr << [i,j] if self[i] + self[j] == 0 
+          end
+        end
+        arr  
+    end
+end  
+
+def my_transpose(arr)
+  new_arr = Array.new(arr.length){Array.new(arr[0].length)}
+  (0...arr.length).each do |i|
+    (0...arr.length).each do |j|
+       new_arr[j][i] = arr[i][j] 
+    end   
+   end
+  new_arr
+end
--- visshal_yizzy_TDD_project/spec/problems_spec.rb
+++ visshal_yizzy_TDD_project/spec/problems_spec.rb
@@ -11,3 +11,34 @@ describe "#my_uniq" do
         expect(my_uniq(arr)).to eq([1,2,3])
     end
 end
+
+describe "#two_sum" do
+    subject(:arr) {arr = [-1,0,2,-2,1]}
+    
+    it "returns a new array" do     
+        expect(arr.two_sum.is_a?(Array)).to eq(true)
+    end
+
+    it "finds the indices of all pairs that sum to zero" do
+        expect(arr.two_sum).to eq([[0,4],[2,3]])
+    end
+
+end
+
+describe "#my_transpose" do
+  subject(:rows){rows = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+  ]}
+
+
+  it "returns a new array" do
+    expect(my_transpose(rows).length).to eq(rows.length) 
+  end
+
+  it "returns an array with the same number" do
+
+    expect(my_transpose(rows)).to eq(rows.transpose)
+  end
+end
