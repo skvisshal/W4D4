8fcb6fffe6029e81bc071e12eedbeb46d71ce1e5
Visshal Suresh <skvisshal@gmail.com>
2021-10-28 14:28:25 -0400

    Complete TDD Project

--- visshal_yizzy_TDD_project/lib/problems.rb
+++ visshal_yizzy_TDD_project/lib/problems.rb
@@ -27,3 +27,55 @@ def my_transpose(arr)
    end
   new_arr
 end
+
+def stock_picker(arr)
+    new_arr = []
+    max = 0
+    (0...arr.length-1).each do |i|
+        (i+1...arr.length).each do |j|
+            if arr[j] - arr[i] > max
+                max = arr[j] - arr[i]
+                new_arr = [i,j]
+            end
+        end
+    end
+    new_arr
+end
+
+class Towers
+
+    attr_accessor :tower_1, :tower_2, :tower_3
+    def initialize
+        @tower_1 = [4,3,2,1]
+        @tower_2 = []
+        @tower_3 = []
+        @tower_hash = {
+            1 => @tower_1,
+            2 => @tower_2,
+            3 => @tower_3
+        }
+    end
+
+    def move(from_tower, to_tower)
+        to_tower << from_tower.pop
+    end
+
+    def won?
+        @tower_1 == [] && @tower_2 == [] && @tower_3 = [4,3,2,1]
+    end
+
+    def play
+
+        until self.win?
+            self.turn
+        end
+        puts "You Won"
+    end
+
+    def turn 
+        puts "Enter from and dest towers like 1 2"
+        response = gets.chomp.split.map(&:to_i)
+        self.move(@tower_hash[response[0]],@tower_hash[response[1]])
+    end
+
+end
--- visshal_yizzy_TDD_project/spec/problems_spec.rb
+++ visshal_yizzy_TDD_project/spec/problems_spec.rb
@@ -42,3 +42,55 @@ describe "#my_transpose" do
     expect(my_transpose(rows)).to eq(rows.transpose)
   end
 end
+
+describe "#stock_picker" do
+
+    subject(:arr) {arr = [3, 1, 5, 2, 3, 7]}
+
+    it "returns a new array" do     
+        expect(stock_picker(arr).is_a?(Array)).to eq(true)
+    end
+
+    it "returns the most profitable pair" do 
+        expect(stock_picker(arr)).to eq([1,5])
+    end
+
+end
+
+describe Towers do
+
+    subject(:towers) {Towers.new}
+
+    describe "#initialize" do
+        it "should initialize towers to an array" do 
+            expect(towers.tower_1).to eq([4,3,2,1])
+            expect(towers.tower_2).to eq([])
+            expect(towers.tower_3).to eq([])
+        end
+    end
+
+    describe "#move" do
+
+        it "should recieve 2 arrays" do
+            expect{towers.move(towers.tower_1,towers.tower_2)}.not_to raise_error
+        end
+
+        it  "should move correctly" do 
+            towers.move(towers.tower_1,towers.tower_2)
+            expect(towers.tower_1).to eq([4,3,2])
+            expect(towers.tower_2).to eq([1])
+        end
+    end
+
+    describe "#won?" do
+        it  "should have all disks in order in 3rd array" do 
+            towers.tower_1 = []
+            towers.tower_2 = []
+            towers.tower_3 = [4,3,2,1]
+            towers.won?
+            expect(towers.tower_1).to eq([])
+            expect(towers.tower_2).to eq([])
+            expect(towers.tower_3).to eq([4,3,2,1])
+        end
+    end
+end
